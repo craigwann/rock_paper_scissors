ee4252ff658f6f730767333bb4c77dafb87fd260
Craig Wann and Rita Bennett-Chew <rita@>
2018-06-27 14:24:35 -0700

    add rps-game.rb to play RPS in terminal

--- rps-game.rb
+++ rps-game.rb
@@ -6,9 +6,16 @@ if game_type == 'p'
   puts("Enter the input for Player One")
   player_one_input = gets().chomp().downcase()
   puts `clear`
-  puts("Enter the input for Player One")
+  puts("Enter the input for Player Two")
   player_two_input = gets().chomp().downcase()
   puts `clear`
   new_game = RPS.new(player_one_input, player_two_input)
   puts("Did player 1 win?",  new_game.first_player_win?())
+
+elsif game_type == 'c'
+  puts("Enter the input for the player")
+  player_input = gets().chomp().downcase()
+  computer_input = ["rock", "paper", "scissors"].shuffle()[0]
+  new_game = RPS.new(player_input, computer_input)
+  puts("The computer played " + computer_input + " Did you win? ", new_game.first_player_win?())
 end
